eb3d1e48cdc7a013e84a0e6c40689d48209f35a9
dymafr <erwan1309@hotmail.com>
2022-07-06 11:13:45 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useTrackMouse } from './hooks/useTrackMouse';
 
 function App() {
+  const position = useTrackMouse();
+
   return (
     <div
       className="d-flex justify-content-center align-items-center"
       style={{ backgroundColor: '#fefefe', height: '100vh', width: '100%' }}
     >
-      <h1>Hello World</h1>
+      <h1>
+        {position.x} : {position.y}
+      </h1>
     </div>
   );
 }
