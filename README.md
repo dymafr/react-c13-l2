e6779d6d24445e733a237eb4e2da661c9c39cd75
dymafr <erwan1309@hotmail.com>
2022-07-06 11:01:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kbaocj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kbaocj)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ backgroundColor: '#fefefe', height: '100vh', width: '100%' }}
+    >
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
